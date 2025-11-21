fb2465c55a386a6632352e933d55cc874ad2b39f
Flora <flora.ortiz@ucb.edu.bo>
2025-11-21 11:25:12 -0400

    feat: AppDbContext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Xml.Linq;
+using Tarea_17_11.Models;
+
+
+namespace Tarea_17_11.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students => Set<Student>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Student>();
+        }
+    }
+}
