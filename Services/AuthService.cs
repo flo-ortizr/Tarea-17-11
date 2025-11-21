41dff7c10e03e46f04c7dc1a9d0f7aef7149ad68
Flora <flora.ortiz@ucb.edu.bo>
2025-11-21 11:53:03 -0400

    feat: AuthService

--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -1,6 +1,28 @@
-﻿namespace Tarea_17_11.Services
+﻿using Tarea_17_11.Models.DTOs;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Tarea_17_11.Services
 {
-    public class AuthService
+    public class AuthService : IAuthService
     {
+        public Task<(bool ok, LoginResponse.LoginResponseDto? response)> LoginAsync(LoginDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<(bool ok, LoginResponse.LoginResponseDto? response)> RefreshAsync(LoginResponse.RefreshRequestDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> RegisterAsync(RegisterDto dto)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
