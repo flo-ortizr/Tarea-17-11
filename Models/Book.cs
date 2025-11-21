3e25c99ba845d70f0ed0441a803640219e5405dc
Flora <flora.ortiz@ucb.edu.bo>
2025-11-21 12:37:26 -0400

    entity nook

---
+++ Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace Tarea_17_11.Models
+{
+    public class Book
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string Genre { get; set; }
+        public string Author { get; set; }
+    }
+}
