240c6ae96b036b0ce5a1d8e1ab9594a073bd4508
Flora <flora.ortiz@ucb.edu.bo>
2025-11-21 13:14:07 -0400

    feat: book controller

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -1,6 +1,58 @@
-﻿namespace Tarea_17_11.Controllers
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Tarea_17_11.Models;
+using Tarea_17_11.Models.DTOs;
+using Tarea_17_11.Services;
+
+namespace Tarea_17_11.Controllers
 {
-    public class BookController
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class BookController : ControllerBase
     {
+        private readonly IBookService _service;
+        public BookController(IBookService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllBooks()
+        {
+            IEnumerable<Book> items = await _service.GetAll();
+            return Ok(items);
+        }
+        [HttpGet("{id:guid}")]
+        [Authorize]
+        public async Task<IActionResult> GetOne(Guid id)
+        {
+            var hospital = await _service.GetOne(id);
+            return Ok(hospital);
+        }
+        [HttpPost]
+        [Authorize(Policy = "AdminOnly")]
+        public async Task<IActionResult> CreateBook([FromBody] CreateBookDto dto)
+        {
+            if (!ModelState.IsValid) return ValidationProblem(ModelState);
+            var hospital = await _service.CreateBook(dto);
+            return CreatedAtAction(nameof(GetOne), new { id = hospital.Id }, hospital);
+        }
+        [HttpPut("{id:guid}")]
+        [Authorize(Policy = "AdminOnly")]
+        public async Task<IActionResult> UpdateBook([FromBody] UpdateBookDto dto, Guid id)
+        {
+            if (!ModelState.IsValid) return ValidationProblem(ModelState);
+            var hospital = await _service.UpdateBook(dto, id);
+            return CreatedAtAction(nameof(GetOne), new { id = hospital.Id }, hospital);
+        }
+
+        [HttpDelete("{id:guid}")]
+        [Authorize(Policy = "AdminOnly")]
+        public async Task<IActionResult> DeleteBook(Guid id)
+        {
+            if (!ModelState.IsValid) return ValidationProblem(ModelState);
+            await _service.DeleteBook(id);
+            return NoContent();
+        }
     }
 }
