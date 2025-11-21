fbabcef5b4ffcff3ab035147faf716ac6143280a
Flora <flora.ortiz@ucb.edu.bo>
2025-11-21 12:52:49 -0400

    feat: book repository interface

---
+++ Repositories/IBookRepository.cs
@@ -0,0 +1,13 @@
+﻿using Tarea_17_11.Models;
+
+namespace Tarea_17_11.Repositories
+{
+    public interface IBookRepository
+    {
+        Task<IEnumerable<Book>> getAll();
+        Task<Book> getOne(Guid id);
+        Task Add(Book book);
+        Task Update(Book book);
+        Task Delete(Book book);
+    }
+}
