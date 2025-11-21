4f344e31da54f8535d6709a7750eeffde471d0b7
Flora <flora.ortiz@ucb.edu.bo>
2025-11-21 11:03:27 -0400

    feat: student model

---
+++ Models/Student.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tarea_17_11.Models
+{
+    public class Student
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100)]
+        public string Username { get; set; } = string.Empty;
+
+        public string PasswordHash { get; set; }
+        public string Role { get; set; } = "User";//"User" | "Admin"
+        public string? RefreshToken { get; set; }
+        public DateTime? RefreshTokenExpiresAt { get; set; }
+        public DateTime? RefreshTokenRevokedAt { get; set; }
+        public string? CurrentJwtId { get; set; }
+
+    }
+}
