890dbb3d6cf586b20f5192e5d8e0fb94af49705e
Flora <flora.ortiz@ucb.edu.bo>
2025-11-21 12:40:15 -0400

    feat: entity user

---
+++ Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace Tarea_17_11.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string Role { get; set; } = "User";//"User" | "Admin"
+        public string? RefreshToken { get; set; }
+        public DateTime? RefreshTokenExpiresAt { get; set; }
+        public DateTime? RefreshTokenRevokedAt { get; set; }
+        public string? CurrentJwtId { get; set; }
+    }
+}
