f3deea61d3b897886f424fc631515c0b0a8b7971
Flora <flora.ortiz@ucb.edu.bo>
2025-11-21 11:06:58 -0400

    feat: register (sign in) dto

--- Models/DTOs/RegisterDto.cs
+++ Models/DTOs/RegisterDto.cs
@@ -1,6 +1,11 @@
 ﻿namespace Tarea_17_11.Models.DTOs
 {
-    public class RegisterDto
+    public record RegisterDto
     {
+        public string Name { get; init; }
+        public string Username { get; init; }
+        public string Email { get; init; }
+        public string Password { get; init; }
+        public string Role { get; set; } = "User";
     }
 }
