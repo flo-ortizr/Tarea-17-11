9101e6a019cae39aa5af2893918ebd442b2c6c4a
Flora <flora.ortiz@ucb.edu.bo>
2025-11-21 12:59:04 -0400

    feat: user repository

--- Repositories/IUserRepository.cs
+++ Repositories/IUserRepository.cs
@@ -6,7 +6,6 @@ namespace Tarea_17_11.Repositories
     {
         Task<User?> GetById(Guid id);
         Task<User?> GetByEmailAddress(string email);
-        Task<User?> GetByRefreshToken(string refreshToken);
         Task AddAsync(User user);
         Task UpdateAsync(User user);
     }
--- Repositories/UserRepository.cs
+++ Repositories/UserRepository.cs
@@ -1,6 +1,36 @@
-﻿namespace Tarea_17_11.Repositories
+﻿using Microsoft.EntityFrameworkCore;
+using Tarea_17_11.Data;
+using Tarea_17_11.Models;
+
+namespace Tarea_17_11.Repositories
 {
-    public class UserRepository
+    public class UserRepository : IUserRepository
     {
+        private readonly AppDbContext _db;
+        public UserRepository(AppDbContext db)
+        {
+            _db = db;
+        }
+        public async Task AddAsync(User user)
+        {
+            await _db.Users.AddAsync(user);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task<User?> GetByEmailAddress(string email)
+        {
+            return await _db.Users.FirstOrDefaultAsync(u => u.Email == email);
+        }
+
+        public async Task<User?> GetById(Guid id)
+        {
+            return await _db.Users.FirstOrDefaultAsync(u => u.Id == id);
+        }
+
+        public async Task UpdateAsync(User user)
+        {
+            _db.Users.Update(user);
+            await _db.SaveChangesAsync();
+        }
     }
 }
