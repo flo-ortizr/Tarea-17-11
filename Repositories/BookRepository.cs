9e53d47d506da6b3527fe01fd585a21f6d20a146
Flora <flora.ortiz@ucb.edu.bo>
2025-11-21 12:55:31 -0400

    feat: book repository

--- Repositories/BookRepository.cs
+++ Repositories/BookRepository.cs
@@ -1,6 +1,42 @@
-﻿namespace Tarea_17_11.Repositories
+﻿using Microsoft.EntityFrameworkCore;
+using Tarea_17_11.Data;
+using Tarea_17_11.Models;
+
+namespace Tarea_17_11.Repositories
 {
-    public class BookRepository
+    public class BookRepository : IBookRepository
     {
+        private readonly AppDbContext _db;
+        public BookRepository(AppDbContext db)
+        {
+            _db = db;
+        }
+        public async Task Add(Book book)
+        {
+            await _db.Books.AddAsync(book);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task Delete(Book book)
+        {
+            _db.Books.Remove(book);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Book>> getAll()
+        {
+            return await _db.Books.ToListAsync();
+        }
+
+        public async Task<Book?> getOne(Guid id)
+        {
+            return await _db.Books.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task Update(Book book)
+        {
+            _db.Books.Update(book);
+            await _db.SaveChangesAsync();
+        }
     }
 }
--- Repositories/IBookRepository.cs
+++ Repositories/IBookRepository.cs
@@ -5,7 +5,7 @@ namespace Tarea_17_11.Repositories
     public interface IBookRepository
     {
         Task<IEnumerable<Book>> getAll();
-        Task<Book> getOne(Guid id);
+        Task<Book?> getOne(Guid id);
         Task Add(Book book);
         Task Update(Book book);
         Task Delete(Book book);
