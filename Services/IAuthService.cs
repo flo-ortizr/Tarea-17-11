edff094e5a433425a3540ff3a4b8c267460ccaa1
Flora <flora.ortiz@ucb.edu.bo>
2025-11-21 11:35:25 -0400

    feat: AuthService interface

---
+++ Services/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using Tarea_17_11.Models.DTOs;
+using static Tarea_17_11.Models.DTOs.LoginResponse;
+
+namespace Tarea_17_11.Services
+{
+    public interface IAuthService
+    {
+        Task<(bool ok, LoginResponseDto? response)> LoginAsync(LoginDto dto);
+        Task<string> RegisterAsync(RegisterDto dto);
+        Task<(bool ok, LoginResponseDto? response)> RefreshAsync(RefreshRequestDto dto);
+    }
+}
