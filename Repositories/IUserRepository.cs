d5bb1f38003a78f9459b6703a6015b8851682836
Flora <flora.ortiz@ucb.edu.bo>
2025-11-21 12:56:07 -0400

    feat: user repository interface

--- Repositories/IUserRepository.cs
+++ Repositories/IUserRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace Tarea_17_11.Repositories
+﻿using Tarea_17_11.Models;
+
+namespace Tarea_17_11.Repositories
 {
     public interface IUserRepository
     {
+        Task<User?> GetById(Guid id);
+        Task<User?> GetByEmailAddress(string email);
+        Task<User?> GetByRefreshToken(string refreshToken);
+        Task AddAsync(User user);
+        Task UpdateAsync(User user);
     }
 }
